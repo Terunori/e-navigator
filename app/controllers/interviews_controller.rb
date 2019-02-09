3c75771f196bfa7db02412e2292c897f6a330c4c
teru <18109251+Terunori@users.noreply.github.com>
2019-02-09 16:24:05 +0900

    new and create interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,6 +1,48 @@
 class InterviewsController < ApplicationController
+  before_action :set_my_thread, only: [:update,:edit,:destroy]
+
+  def new
+    @interview = Interview.new
+  end
+
+  def edit
+
+  end
 
   def show
+
+  end
+
+  def update
+    if @interview.update(interview_params)
+      redirect_to user_interviews_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @interview.destroy
+    redirect_to user_interviews_path
+  end
+
+  def create
+    @interview = Interview.create(interview_params)
+    if @interview.save
+      redirect_to user_interviews_path
+    else
+      # flash[:alert] = 'failed'
+      render 'new'
+    end
+  end
+
+  private
+  def interview_params
+    params.permit(:user_id, :begin_at, :allowed, :num)
+  end
+
+  def set_interview
+    @interview = Interview.find(params[:num])
   end
 
 end
