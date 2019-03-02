71f0af281b21a0b98e55ae45e32a769c7291f3fb
teru <18109251+Terunori@users.noreply.github.com>
2019-03-02 19:41:36 +0900

    send email when allow or cancel interview

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,5 @@
 class InterviewsController < ApplicationController
-  before_action :set_user, only: %i[index new create edit update destroy notify_request]
+  before_action :set_user, only: %i[index new create edit update allow destroy notify_request]
   before_action :correct_user, only: %i[new create edit update destroy notify_request]
   before_action :set_interview, only: %i[update allow edit destroy]
 
@@ -32,6 +32,8 @@ class InterviewsController < ApplicationController
     @interview.interviewer_id = nil if allow_params[:allowed] == 'undecided'
 
     if @interview.save
+      notify_cancel if allow_params[:allowed] == 'undecided'
+      notify_allow if allow_params[:allowed] == 'allowed'
       redirect_to user_interviews_path
     else
       render 'edit'
@@ -55,27 +57,13 @@ class InterviewsController < ApplicationController
 
   def notify_request
     @interviewer = User.find(request_params[:interviewer_id])
-    @user = current_user
     if @user.interviews.where(allowed: 'undecided').present?
-      InterviewMailer.request_schedule_email(@interviewer, @user).deliver_later
+      InterviewMailer.request(@interviewer, @user).deliver_later
       flash[:notice] = (@interviewer.name_email) + '様に申請が完了しました'
     else
       flash[:alert] = '有効な日程がありません　新規日程追加より面接を希望する日時を設定してください'
     end
     redirect_to user_interviews_path(@user)
-  end
-
-  def apply_request
-    @interviewer = current_user
-    @interviewee = User.find(params[:user_id])
-    @interviews = @interviewee.interviews.where(allowed: 'allowed', interviewer_id: @interviewer.id)
-    if @interviews.present?
-      InterviewMailer.apply_schedule_email(@interviewer, @interviewee).deliver_later
-      flash[:notice] = '承認した日程を通知しました'
-    else
-      flash[:alert] = '日程を承認してください'
-    end
-    redirect_to user_interviews_path(@interviewee)
   end
 
   private
@@ -89,6 +77,16 @@ class InterviewsController < ApplicationController
 
   def correct_user
     redirect_to user_interviews_path if current_user != @user
+  end
+
+  def notify_cancel
+    InterviewMailer.cancel(current_user, @user, @interview).deliver_later
+    flash[:notice] = (@user.name_email) + 'さんに承認日程のキャンセルを通知しました'
+  end
+
+  def notify_allow
+    InterviewMailer.allow(current_user, @user, @interview).deliver_later
+    flash[:notice] = (@user.name_email) + 'さんに' + l(@interview.begin_at, format: :long_toM) + 'から始まる面接の承認を通知しました'
   end
 
   def create_params
--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -3,16 +3,23 @@ class InterviewMailer < ApplicationMailer
           bcc: ENV['EMAIL_ADDRESS'],
           reply_to: ENV['EMAIL_ADDRESS']
 
-  def request_schedule_email(interviewer, interviewee)
+  def request(interviewer, interviewee)
     @interviewer = interviewer
     @interviewee = interviewee
     mail(to: interviewer.email, cc: interviewee.email, subject: '【e-navigator】面接希望日が決まりました')
   end
 
-  def apply_schedule_email(interviewer, interviewee)
+  def cancel(interviewer, interviewee, schedule)
     @interviewer = interviewer
     @interviewee = interviewee
-    @interview_schedules = @interviewee.interviews.where(allowed: 'allowed', interviewer_id: @interviewer.id).order(begin_at: :asc)
+    @schedule = schedule
+    mail(to: interviewee.email, cc: interviewer.email, subject: '【e-navigator】面接日程がキャンセルされました')
+  end
+
+  def allow(interviewer, interviewee, schedule)
+    @interviewer = interviewer
+    @interviewee = interviewee
+    @schedule = schedule
     mail(to: interviewee.email, cc: interviewer.email, subject: '【e-navigator】面接日程が承認されました')
   end
 end
