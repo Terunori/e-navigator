1fe1937080d8eeb7d7ff92fe2884340a01952ddb
teru <18109251+Terunori@users.noreply.github.com>
2019-03-02 16:14:17 +0900

    change name of arguments

--- app/mailers/interview_mailer.rb
+++ app/mailers/interview_mailer.rb
@@ -3,10 +3,10 @@ class InterviewMailer < ApplicationMailer
           bcc: ENV['EMAIL_ADDRESS'],
           reply_to: ENV['EMAIL_ADDRESS']
 
-  def request_schedule_email(send_to, cc)
-    @interviewer = send_to
-    @interviewee = cc
-    mail(to: send_to.email, cc: cc.email, subject: '【e-navigator】面接希望日が決まりました')
+  def request_schedule_email(interviewer, interviewee)
+    @interviewer = interviewer
+    @interviewee = interviewee
+    mail(to: interviewer.email, cc: interviewee.email, subject: '【e-navigator】面接希望日が決まりました')
   end
 
   def apply_schedule_email(interviewer, interviewee)
