5b8b2b46275e85b6455603b5f3a99161e127bc0b
teru <18109251+Terunori@users.noreply.github.com>
2019-03-04 18:15:51 +0900

    set redirect when login
    set redirect when login
    
    set redirect when login

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    authenticated :user do
-      root :to => 'users#index'
-      delete 'logout', to: 'devise/sessions#destroy'
-      resources :users, only: [:index, :show, :edit, :update] do
-        resources :interviews do
-          member do
-            patch :allow, as: 'allow'
-          end
-          collection do
-            post :notify_request
-          end
+    root :to => 'users#index'
+    delete 'logout', to: 'devise/sessions#destroy'
+    resources :users, only: [:index, :show, :edit, :update] do
+      resources :interviews do
+        member do
+          patch :allow, as: 'allow'
+        end
+        collection do
+          post :notify_request
         end
       end
-    end
-    unauthenticated :user do
-      root :to => 'devise/sessions#new', as: :unauthenticated_root
     end
   end
 end
